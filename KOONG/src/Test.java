f26ba2a353bb200777f246cd47465e9bf3a68630
smhrd <smhrd@DESKTOP-8UKO09N>
2022-09-02 15:48:16 +0900

    세번째 푸쉬/민욱

--- KOONG/src/Test.java
+++ KOONG/src/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("First Commit!");
 		System.out.println("야채부락리 야구 시작!!");
-		
+		System.out.println("제가 올려보겠습니다.");
 		
 	}
 
